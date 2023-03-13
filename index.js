bafc0fa6f99d137632f0a6fa89b269ffe5f38741
waltersimekha <waltersimekha@gmail.com>
2023-03-13 21:37:32 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
 
 // Write your code here
+
+// Write your code here
+let num1 = 31;
+let num2 = 2;
+const multiply= num1* num2;
+
+
+let random= Math.floor(Math.random() * 10) + 1;
+
+let num3= 32;
+let num4= 7;
+const mod= num3%num4;
+
+const max = Math.max(5, 10 ,11,12,17,19 ,20)
